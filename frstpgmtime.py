d4267beb179259f008c4d022fd37eb9e12a12cea
VARUN SAKUNIA <54551996+Varun0801@users.noreply.github.com>
2020-04-06 12:46:04 +0530

    Add files via upload

---
+++ frstpgmtime.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 19:00:23 2020
+
+@author: Administrator
+"""
+
+
+import time
+a = time.time()
+#f = time.localtime(a)
+t = time.ctime(a)
+print(t)
+#print(f)
---
+++ secndtime.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 19:15:15 2020
+
+@author: Administrator
+"""
+
+
+from datetime import *
+'''a = datetime.now()
+print(a)'''
+a = datetime.today()
+print(a)
+s = date.today()
+print(s)
+
